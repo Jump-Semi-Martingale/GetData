60d0eb6cca51d124c22038f42b2fac3fea82c9af
Daiya-Mita <daiyamaxus@gmail.com>
2016-07-02 19:17:08 +0900

    関係ないデータだけど折れ線グラフ追加

--- memo_test-master/memo_test/InputViewController.swift
+++ memo_test-master/memo_test/InputViewController.swift
@@ -7,17 +7,22 @@
 //
 
 import UIKit
+import Charts
 
 class InputViewController: UIViewController {
 
     
     
+    @IBOutlet weak var lineChartView: LineChartView!
     
     
     
     @IBOutlet weak var TextView5: UITextView!
 
     var vc2Text:String?
+    
+    var months: [String]!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,12 +32,45 @@ class InputViewController: UIViewController {
             // ViewControllerからのメッセージを表示する
             TextView5.text = vc2Text
         }
+        
+        
+        
+        
+        months = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
+        let unitsSold = [50.3, 68.3, 113.3, 115.7, 160.8, 214.0, 220.4, 132.1, 176.2, 120.9, 71.3, 48.0]
+        
+        lineChartView.animate(yAxisDuration: 2.0)
+        lineChartView.descriptionText = "京都府の月毎の降水量グラフ"
+        
+        setChart(months, values: unitsSold)
+
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        lineChartView.noDataText = "You need to provide data for the chart."
+        
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = LineChartDataSet(yVals: dataEntries, label: "降水量")
+        let chartData = LineChartData(xVals: months, dataSet: chartDataSet)
+        lineChartView.data = chartData
+    }
+    
+
     
  
 
